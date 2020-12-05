1bc5fa255dc86e0caf85bc0cab31c21d0691ee98
Abror Khoerun Nizam <abror.nizam29@gmail.com>
2020-12-05 21:20:49 +0700

    Remove unused route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
+Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index');
 
 Route::get('/testing', function () {
